e1ac3db0dbb3742eced0dc7c67dcd3768548e3da
p0ns <pons@p0ns.org>
2017-12-18 21:49:17 -0300

    needs that S, someone pls tell me why

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -141,11 +141,11 @@ public:
 
 	checkpointData = (CCheckpointData) {
 		boost::assign::map_list_of
-      (  0, uint256("0x6e27bffd2a104bea1c870be76aab1cce13bebb0db40606773827517da9528174")) //GENESIS
-      (  1, uint256("0xeadcbcb3f8960778b1c39415d1447a5cb45981ac15b269e31924f5f4701ea0e8")) //PREMINE
-      ( 2833, uint256("0xe281b0a49fd63613fd9ceffc758cd2044f72278cc9606fc7c7f86422d977e44f")) //POOL
-      ( 50000, uint256("0x1f676ba037561706283a5cdac53d20c4d81576faffb4940ea3f0ddaa3a91f04b")) //BLOCK 50.000
-      ( 80000, uint256("0x36813083a53ef7dc73037a92b245fbdced69f091bb8549da520331f159fdbf6b")), //BLOCK 80.000
+      (  0, uint256S("0x6e27bffd2a104bea1c870be76aab1cce13bebb0db40606773827517da9528174")) //GENESIS
+      (  1, uint256S("0xeadcbcb3f8960778b1c39415d1447a5cb45981ac15b269e31924f5f4701ea0e8")) //PREMINE
+      ( 2833, uint256S("0xe281b0a49fd63613fd9ceffc758cd2044f72278cc9606fc7c7f86422d977e44f")) //POOL
+      ( 50000, uint256S("0x1f676ba037561706283a5cdac53d20c4d81576faffb4940ea3f0ddaa3a91f04b")) //BLOCK 50.000
+      ( 80000, uint256S("0x36813083a53ef7dc73037a92b245fbdced69f091bb8549da520331f159fdbf6b")), //BLOCK 80.000
 			1509752962, // * UNIX timestamp of last checkpoint block
 			91072,   // * total number of transactions between genesis and last checkpoint
 			//   (the tx=... number in the SetBestChain debug.log lines)
