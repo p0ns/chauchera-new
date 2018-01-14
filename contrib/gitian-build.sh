83b2a89d60648c571e98d7a37d17863c45c37184
p0ns <pons@p0ns.org>
2018-01-13 23:40:08 -0300

    fix repo chauchera en doc

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -17,7 +17,7 @@ osx=true
 SIGNER=
 VERSION=
 commit=false
-url=https://github.com/chaucha-project/chaucha
+url=https://github.com/proyecto-chaucha/chauchera
 proc=2
 mem=2000
 lxc=true
@@ -39,7 +39,7 @@ version		Version number, commit, or branch to build. If building a commit or bra
 
 Options:
 -c|--commit	Indicate that the version argument is for a commit or branch
--u|--url	Specify the URL of the repository. Default is https://github.com/chaucha-project/chaucha
+-u|--url	Specify the URL of the repository. Default is https://github.com/proyecto-chaucha/chauchera
 -v|--verify 	Verify the gitian build
 -b|--build	Do a gitiain build
 -s|--sign	Make signed binaries for Windows and Mac OSX
@@ -233,7 +233,7 @@ if [[ $setup = true ]]
 then
     sudo apt-get install ruby apache2 git apt-cacher-ng python-vm-builder qemu-kvm qemu-utils
     git clone https://github.com/chaucha-project/gitian.sigs.ltc.git
-    git clone https://github.com/chaucha-project/chaucha-detached-sigs.git
+    git clone https://github.com/proyecto-chaucha/chauchera-detached-sigs.git
     git clone https://github.com/devrandom/gitian-builder.git
     pushd ./gitian-builder
     if [[ -n "$USE_LXC" ]]
--- doc/release-notes-litecoin.md
+++ doc/release-notes-litecoin.md
@@ -7,7 +7,7 @@ It is recommended to upgrade to this version.
 
 Please report bugs using the issue tracker at github:
 
-  <https://github.com/chaucha-project/chaucha/issues>
+  <https://github.com/proyecto-chaucha/chauchera/issues>
 
 Compatibility
 ==============
@@ -64,7 +64,7 @@ Testnet faucets can be located at:
 - http://testnet.chauchatools.com
 - http://testnet.thrasher.io
 
-Developers who require the new testnet blockchain paramaters can find them [here](https://github.com/chaucha-project/chaucha/blob/0.13/src/chainparams.cpp#L214).
+Developers who require the new testnet blockchain paramaters can find them [here](https://github.com/proyecto-chaucha/chauchera/blob/0.13/src/chainparams.cpp#L214).
 
 Credits
 =======
