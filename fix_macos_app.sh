53382b6605bf4a100f6db1433708a9aca422478d
César <heellxz@gmail.com>
2019-05-07 20:32:53 -0400

    remove innecesary files

--- fix_macos_app.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-dylibbundler -od -b -x ./Chauchera.app/Contents/MacOS/Chauchera -d ./Chauchera.app/Contents/libs/ -p @executable_path/../libs/
-/usr/local/opt/qt/bin/macdeployqt Chauchera.app/
