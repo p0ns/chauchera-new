0666e163858e03fd1a85dfd685a4c1cc5592bc15
cvasqxz <heellxz@gmail.com>
2019-05-08 14:47:58 -0400

    RPC Ports mainnet/testnet/regtest

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -33,7 +33,7 @@ class CBaseMainParams : public CBaseChainParams
 public:
     CBaseMainParams()
     {
-        nRPCPort = 9332;
+        nRPCPort = 21662;
     }
 };
 
@@ -45,7 +45,7 @@ class CBaseTestNetParams : public CBaseChainParams
 public:
     CBaseTestNetParams()
     {
-        nRPCPort = 19332;
+        nRPCPort = 31662;
         strDataDir = "testnet4";
     }
 };
@@ -58,7 +58,7 @@ class CBaseRegTestParams : public CBaseChainParams
 public:
     CBaseRegTestParams()
     {
-        nRPCPort = 19443;
+        nRPCPort = 19442;
         strDataDir = "regtest";
     }
 };
