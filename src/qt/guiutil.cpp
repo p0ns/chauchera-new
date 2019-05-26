17220fa0d0fb2e76836669a0178a8974c3b42dd9
cvasqxz <heellxz@gmail.com>
2019-05-26 18:59:47 -0400

    Rebranding #6 - URI
    litecoin: a chaucha:

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -210,9 +210,9 @@ bool parseBitcoinURI(QString uri, SendCoinsRecipient *out)
     //
     //    Cannot handle this later, because bitcoin:// will cause Qt to see the part after // as host,
     //    which will lower-case it (and thus invalidate the address).
-    if(uri.startsWith("litecoin://", Qt::CaseInsensitive))
+    if(uri.startsWith("chaucha://", Qt::CaseInsensitive))
     {
-        uri.replace(0, 11, "litecoin:");
+        uri.replace(0, 11, "chaucha:");
     }
     QUrl uriInstance(uri);
     return parseBitcoinURI(uriInstance, out);
@@ -220,7 +220,7 @@ bool parseBitcoinURI(QString uri, SendCoinsRecipient *out)
 
 QString formatBitcoinURI(const SendCoinsRecipient &info)
 {
-    QString ret = QString("litecoin:%1").arg(info.address);
+    QString ret = QString("chaucha:%1").arg(info.address);
     int paramCount = 0;
 
     if (info.amount)
